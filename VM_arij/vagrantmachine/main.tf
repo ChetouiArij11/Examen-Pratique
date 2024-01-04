a7165716f0b81f8d6e8c71314c1f697aac5bf4fe
ChetouiArij <99617038+ChetouiArij11@users.noreply.github.com>
2024-01-04 19:29:22 +0100

    Update main.tf

--- VM_arij/vagrantmachine/main.tf
+++ VM_arij/vagrantmachine/main.tf
@@ -1,13 +1,25 @@
-provider "docker" {
-  host = "tcp://192.168.121.213:2375"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
 }
 
-resource "docker_container" "salutation" {
-  name  = "salutation"
-  image = "arijchetoui1/testautomatisation:latest"
+provider "docker" {host = "tcp://192.168.121.138:2375"}
 
+resource "docker_image" "testautomatisation" {
+  name         = "arijchetoui1/testautomatisation"
+  keep_locally = false
+}
+
+resource "docker_container" "testautomatisation" {
+  image = docker_image.testautomatisation.name
+  name  = "testautomatisation"
   ports {
     internal = 8888
     external = 9999
   }
+  restart = "unless-stopped"  # Politique de redémarrage
 }
